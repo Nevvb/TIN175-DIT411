974e64bb6ad7a20a9ca5d12af215078a5a80508a
oskery <oskar.lyrstrand@gmail.com>
2019-02-10 16:08:22 +0100

    Move left if angle < 0 else move right

--- test01.py
+++ test01.py
@@ -1,15 +1,27 @@
-import sys
 import gym
-env = gym.make('CartPole-v0')
-#env = gym.make('MountainCar-v0')
-#env = gym.make('MsPacman-v0')
-#env = gym.make('LunarLander-v2')
-#env = gym.make('SpaceInvaders-v0')
-#env = gym.make('FrozenLake-v0')
-env.reset()
-for _ in range(20):
-    for _ in range(100):
-        env.render()
-        env.step(env.action_space.sample()) # take a random action
-    env.reset()
-sys.exit()
+import random
+
+env_name = "CartPole-v1"
+env = gym.make(env_name)
+print("Observation space:", env.observation_space)
+print("Action space:", env.action_space)
+
+class Agent():
+    def __init__(self, env):
+        self.action_size = env.action_space.n
+        print("Action size:", self.action_size)
+        
+    def get_action(self, state):
+#         action = random.choice(range(self.action_size))
+        pole_angle = state[2]
+        action = 0 if pole_angle < 0 else 1
+        return action
+
+agent = Agent(env)
+state = env.reset()
+
+for _ in range(200):
+#     action = env.action_space.sample()
+    action = agent.get_action(state)
+    state, reward, done, info = env.step(action)
+    env.render()
