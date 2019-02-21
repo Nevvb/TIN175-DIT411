2ad510cb59313086040f99aea4ce00fd61588a48
Torbjornsson <david.torbjornsson@gmail.com>
2019-02-21 15:08:20 +0100

    test q-learning with taxi

---
+++ main.py
@@ -0,0 +1,61 @@
+import gym
+import random
+import numpy as np
+
+print("Gym:", gym.__version__)
+
+env_name = "Taxi-v2"
+env = gym.make(env_name).env
+print("Observation space:", env.observation_space)
+print("Action space:", env.action_space)
+
+q_table = np.zeros([env.observation_space.n, env.action_space.n])
+
+#Hyperparameters
+alpha = 0.1
+gamma = 0.6
+epsilon = 0.1
+
+#for plotting metrcis
+all_epochs = []
+all_penalties = []
+
+num_episodes = 20
+
+for ep in range(num_episodes):
+    state = env.reset()
+
+    epochs, penalties, reward = 0, 0, 0
+
+    done = False
+
+    for  i in range(100):
+        #Explore
+        if random.uniform(0, 1) < epsilon:
+            action = env.action_space.sample()
+        else:
+            action = np.argmax(q_table[state])
+
+        next_state, reward, done, info = env.step(action)
+
+        old_value = q_table[state, action]
+        next_max = np.max(q_table[next_state])
+
+        new_value = (1 - alpha) * old_value + alpha * (reward + gamma *next_max)
+        q_table[state, action] = new_value
+
+        if reward == -10:
+            penalties += 1
+
+        state = next_state
+
+        epochs += 1
+        env.render()
+        print(state)
+        
+        
+        if done:
+            print('Episode finished after {} timesteps' .format(i+1))
+            break
+
+print('success')
