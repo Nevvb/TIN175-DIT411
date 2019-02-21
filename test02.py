22f908a721a032d3696d5ca969cf3e5cee7dcbab
bob <robin.lilius@gmail.com>
2019-02-21 11:17:44 +0100

    Second try

--- test02.py
+++ test02.py
@@ -11,11 +11,6 @@ class State:
         self.size = len(self.states)
         self.action = action
 
-        for i in range(self.size):
-            # while abs(self.states[i]) < 10:
-            self.states[i] *= 10
-            self.states[i] = round(self.states[i])
-
     def __hash__(self):
         h = 0;
         for value in self.states:
@@ -26,28 +21,39 @@ class State:
     def __str__(self):
         return "State: " + str(self.states) + ", action: " + str(self.action)
 
+    def copy(self):
+        new_state = State(self.states, self.action)
+        return new_state
+
 # --- Specific cartpole q-learning
 
 env = gym.make('CartPole-v0')
 
 q = dict()
 
-high = env.observation_space.high
-low = env.observation_space.low
+size_up = 10
+# high = env.observation_space.high
+# high_multi = list()
+# low = env.observation_space.low
+# low_multi = list()
 actions = env.action_space
 
-for i in range(len(high)):
-    while abs(high[i]) < 10:
-        high[i] *= 10
-    high[i] = round(high[i])
+# for i in range(len(high)):
+#     high_multi.append(0)
+#     while abs(high[i]) < 10:
+#         high[i] *= 10
+#         high_multi[i] += 1
+#     high[i] = round(high[i])
 
-for i in range(len(low)):
-    while abs(low[i]) < 10:
-        low[i] *= 10
-    low[i] = round(low[i])
+# for i in range(len(low)):
+#     low_multi.append(0)
+#     while abs(low[i]) < 10:
+#         low[i] *= 10
+#         low_multi[i] += 1
+#     low[i] = round(low[i])
 
-print("max: "+str(high))
-print("min: "+str(low))
+# print("max: "+str(high))
+# print("min: "+str(low))
 print("actions: "+str(actions))
 
 epsilon = 0.9   # Epsilon greedy probability
@@ -55,7 +61,7 @@ alfa = 0.5      # Learning rate
 gamma = 0.8     # Discount factor
 
 def max_a(state):
-    s = State(state.states, state.action)
+    s = state.copy()
     reward = 0
     best_action = actions.sample()
     for i in range(actions.n):
@@ -67,9 +73,20 @@ def max_a(state):
 
 def get_reward(state):
     reward = 0
-    if best_new_state in q:
-        reward = q[best_new_state]
+    if state in q:
+        reward = q[state]
     return reward
+
+def state_from_space(space, action):
+    l = list()
+    for i in range(len(space)):
+        n = space[i]
+        # for x in range(high_multi[i]):
+        #     n *= 10
+        n *= size_up
+        l.append(int(round(n)))
+    state = State(l, action)
+    return state
 
 # sys.exit()
 
@@ -79,12 +96,14 @@ for i_episode in range(X):
 
     # Resetting environment
     observation = env.reset()
-    old_state = State(observation, actions.sample())
-    new_state = old_state
+    old_state = state_from_space(observation, actions.sample())
+    new_state = old_state.copy()
 
     # Running through an episode
     for t in range(100):
         env.render()
+
+        # print("old state reward: "+str(old_reward)+ ", new reward: "+str(q[old_state]))
 
         # Select action
         action = max_a(old_state)
@@ -96,10 +115,10 @@ for i_episode in range(X):
 
         # Perform action, observe reward and new state
         observation, reward, done, info = env.step(action)
-        new_state = State(observation, action)
+        new_state = state_from_space(observation, action)
 
-        print("Old: "+str(old_state))
-        print("New: "+str(new_state))
+        print("Old: "+str(old_state)+", reward: "+str(get_reward(old_state)))
+        print("New: "+str(new_state)+", reward: "+str(get_reward(new_state)))
 
         # Q-learning equation
         best_new_state = State(new_state.states, max_a(new_state))
@@ -107,10 +126,10 @@ for i_episode in range(X):
         old_reward = get_reward(old_state)
         # print("Best: "+str(best_new_state)+", reward: "+str(best_new_reward))
         q[old_state] = old_reward + alfa * (reward + gamma * best_new_reward - old_reward)
-        print("new reward for old state: "+str(q[old_state]))
+        print("old reward: "+str(old_reward)+ ", new reward: "+str(q[old_state])+", vs function 'get_reward': "+str(get_reward(old_state)))
 
         # Updating state
-        old_state = new_state
+        old_state = new_state.copy()
 
         # Aborting episode if done
         if done:
