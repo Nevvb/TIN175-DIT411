2b8ca8f3e84eb4428e07ab083a260b6398ad846b
Samuel Holgersson <samuel.holgersson@gmail.com>
2019-02-21 19:27:23 +0100

    Fix bad stuff.

--- test02.py
+++ test02.py
@@ -5,29 +5,10 @@ import random
 import numpy as np
 from gym import spaces
 
-# --- State class for q-learning
-
-class State:
-    def __init__(self, state_list):
-        self.states = state_list
-        self.size = len(self.states)
-
-    def __hash__(self):
-        h = 0;
-        for value in self.states:
-            h = h*31 + value*10
-        return int(round(h))
-
-    def __str__(self):
-        return "State: " + str(self.states)
-
-    def copy(self):
-        new_state = State(self.states)
-        return new_state
-
 # --- Specific cartpole q-learning
 
 env = gym.make('CartPole-v0')
+# env = gym.make('MountainCar-v0')
 
 q = dict()
 
@@ -35,9 +16,9 @@ size_up = 10
 actions = env.action_space
 print("actions: "+str(actions))
 
-epsilon = 0.01  # Epsilon greedy probability
+epsilon = 0.1   # Epsilon greedy probability
 alpha = 0.2     # Learning rate
-gamma = 0.8     # Discount factor
+gamma = 0.5     # Discount factor
 
 def max_a(state):
     best_reward = 0.0
@@ -49,9 +30,10 @@ def max_a(state):
                 best_reward = reward
             elif reward == best_reward:
                 best_actions.append(action)
-        print(f'actions: {best_actions}, reward: {best_reward}')
+        if best_reward != 0.0:
+            print(f'actions: {best_actions}, reward: {best_reward}')
         action_chosen = random.randint(0, len(best_actions) - 1)
-        print(f'action chosen: {best_actions[action_chosen]}')
+        # print(f'action chosen: {best_actions[action_chosen]}')
         return best_actions[action_chosen]
     else:
         q[state] = dict()
@@ -60,46 +42,28 @@ def max_a(state):
         return random.randint(0, 1)
 
 def state_from_space(space):
-    print(np.ndarray.round(space, 1))
+    # print(np.ndarray.round(space, 1))
     return str(np.ndarray.round(space, 1))
-    # l = list()
-    # for i in range(len(space)):
-    #     n = space[i]
-    #     # for x in range(high_multi[i]):
-    #     #     n *= 10
-    #     n *= size_up
-    #     l.append(int(round(n)))
-    # state = State(l)
-    # return state
 
 # sys.exit()
 
 # Running X episodes
-X = 100
+X = 1000
 for i_episode in range(X):
 
     # Resetting environment
     observation = env.reset()
     old_state = state_from_space(observation)
-    # new_state = old_state.copy()
-    # print("hash: "+str(old_state.__hash__())+ ", hash2: "+str(old_state.__hash__()))
 
     # Running through an episode
     for t in range(100):
         env.render()
-
-        # print("old state reward: "+str(old_reward)+ ", new reward: "+str(q[old_state]))
 
         # Select action
         if random.random() < epsilon:
             action = env.action_space.sample()
         else:
             action = max_a(old_state)
-        # if old_state in q:
-        #     print("in q!")
-        #     action = q[old_state].action
-        # else:
-        #     action = env.action_space.sample()
 
         # Perform action, observe reward and new state
         observation, reward, done, info = env.step(action)
@@ -110,18 +74,17 @@ for i_episode in range(X):
             q[new_state] = dict()
             for action in [0, 1]: # TODO Same as above
                 q[new_state][action] = 0.0
+
         # Q-learning equation
         q[old_state][action] = (1 - alpha) * q[old_state][action] + alpha * (reward + gamma * q[new_state][max_a(new_state)])
-        print(q[old_state][action])
+        # print(q[old_state][action])
         
-
         # Updating state
         old_state = new_state
-        # print(f'{q.q}\n')
 
         # Aborting episode if done
         if done:
            print("Episode finished after {} timesteps".format(t+1))
-           # break
+           break
 
 sys.exit()
