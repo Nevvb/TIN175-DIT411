3c505eaa26edd39f6ac8066bdc3bee9b7b7fbfea
bob <robin.lilius@gmail.com>
2019-02-21 09:44:21 +0100

    Saving states during loop

--- test02.py
+++ test02.py
@@ -1,5 +1,6 @@
 import sys
 import gym
+import keyboard
 
 from gym import spaces
 
@@ -7,14 +8,14 @@ from gym import spaces
 
 class State:
     def __init__(self, state_list):
-        self.size = len(state_list)
         self.states = state_list
+        self.size = len(self.states)
 
     def __hash__(self):
         h = 0;
-        for value in states:
-            h = hash*31 + hash(value)
-        return h
+        for value in self.states:
+            h = h*31 + value
+        return int(round(h))
 
     def __str__(self):
         return "State: " + str(self.states)
@@ -24,25 +25,50 @@ class State:
 env = gym.make('CartPole-v0')
 
 q = dict()
-# for s in range(env.observation_space.n)
-#     for a in range(env.action_space.n)
-a = {1, 2, 3, 4}
-b = State(a)
-# q[1] = "hej"
-print(b)
 
-sys.exit()
+high = env.observation_space.high
+low = env.observation_space.low
+
+for i in range(len(high)):
+    while abs(high[i]) < 10:
+        high[i] *= 10
+    high[i] = round(high[i])
+
+for i in range(len(low)):
+    while abs(low[i]) < 10:
+        low[i] *= 10
+    low[i] = round(low[i])
+
+print("max: "+str(high))
+print("min: "+str(low))
+
+epsilon = 0.9
+
+# sys.exit()
 
 for i_episode in range(20):
     observation = env.reset()
+    s1 = State(observation)
+    s2 = s1
     for t in range(100):
+        # if keyboard.is_pressed('esc'):
+        #     print("quit!")
+        #     break
         env.render()
-        # env.step(env.action_space.sample()) # take a random action
-        print(observation);
-        #action = env.action_space.sample()
-        action = 0
+
+        print(s1)
+        if s1 in q:
+            print("in q!")
+            action = q[s1]
+        else:
+            action = env.action_space.sample()
+
         observation, reward, done, info = env.step(action)
-        #if done:
-        #    print("Episode finished after {} timesteps".format(t+1))
-        #    break
+        if done:
+           print("Episode finished after {} timesteps".format(t+1))
+           break
+
+        s2 = s1
+        s1 = State(observation)
+
 sys.exit()
