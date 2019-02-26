fed22c348eed127e33c1adc87d23282d266e2b95
Torbjornsson <david.torbjornsson@gmail.com>
2019-02-26 11:18:13 +0100

    breakout!

--- main.py
+++ main.py
@@ -6,27 +6,39 @@ print("Gym:", gym.__version__)
 
 #env_name = "Taxi-v2"
 env_name = "Breakout-v0"
+#env_name = "CartPole-v0"
 env = gym.make(env_name).env
 print("Observation space:", env.observation_space)
 print("Action space:", env.action_space)
+state = env.reset()
 
+
+def toGrayscale(img):
+    return np.mean(img, axis=2).astype(np.uint8)
+
+def downSample(img):
+    return img[::2, ::2]
+
+def preProcess(img):
+    return toGrayscale(downSample(img))
 #q_table = np.zeros([env.observation_space.n, env.action_space.n])
-q_table = np.zeros([300, env.action_space.n])
+#q_table = [state, env.action_space.n]
+q_table = np.zeros([env.observation_space.shape[0], env.action_space.n])
 
 #Hyperparameters
 alpha = 0.1     #Learning rate
-gamma = 0.6     #Accumulated discount reward
+gamma = 1     #Accumulated discount reward
 epsilon = 0.1   #Rate of exploration
 
 #For plotting metrics
 all_epochs = []
+all_rewards = []
 
-
-num_episodes = 2000
-all_rewards = np.zeros([num_episodes])
+num_episodes = 200
+i = 0
 
 for ep in range(num_episodes):
-    i = 0
+    
     state = env.reset()
 
     epochs, penalties, reward, tot_reward = 0, 0, 0, 0
@@ -41,16 +53,16 @@ for ep in range(num_episodes):
         else:
             action = np.argmax(q_table[state])
 
-        next_state, reward, done, info = env.step(action)
+        observation, reward, done, info = env.step(action)
+
+        next_state = preProcess(observation)
+        #print(next_state)
 
         old_value = q_table[state, action]
         next_max = np.max(q_table[next_state])
 
         new_value = (1 - alpha) * old_value + alpha * (reward + gamma *next_max)
         q_table[state, action] = new_value
-
-        if reward == -10:
-            penalties += 1
 
         state = next_state
 
@@ -60,10 +72,16 @@ for ep in range(num_episodes):
         
         
         if done:
-            print('Episode finished after {} timesteps' .format(epochs+1))
-            all_rewards[i] = tot_reward
-            print("all rewards {}".format(all_rewards))
+            print('Episode {}' .format(i), 'finished with {} reward' .format(tot_reward))
+            all_rewards.append(tot_reward)
+            epsilon *= 0.9
+            #print("all rewards {}".format(all_rewards))
+            i += 1
             break
-    i += 1
+    
+with open('test.txt', 'w') as f:
+    for item in all_rewards:
+        f.write("%s\n" % item)
+    f.close
 
-print('success')
+print(all_rewards)
