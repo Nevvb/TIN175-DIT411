9dc79a91e270794ad9e43682611fd9c2c764ca44
Torbjornsson <david.torbjornsson@gmail.com>
2019-02-26 14:32:56 +0100

    print q_table to file

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import gym
 import random
+import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
 print("Gym:", gym.__version__)
 
 #env_name = "Taxi-v2"
-env_name = "Breakout-v0"
+env_name = "BreakoutDeterministic-v4"
 #env_name = "CartPole-v0"
 env = gym.make(env_name).env
 print("Observation space:", env.observation_space)
@@ -23,18 +25,20 @@ def preProcess(img):
     return toGrayscale(downSample(img))
 #q_table = np.zeros([env.observation_space.n, env.action_space.n])
 #q_table = [state, env.action_space.n]
-q_table = np.zeros([env.observation_space.shape[0], env.action_space.n])
+#print(env.observation_space.shape[0]*env.observation_space.shape[1])
+q_table = np.zeros([env.observation_space.shape[0]*env.observation_space.shape[1], env.action_space.n])
+#print(state)
 
 #Hyperparameters
 alpha = 0.1     #Learning rate
 gamma = 1     #Accumulated discount reward
-epsilon = 0.1   #Rate of exploration
+epsilon = 0.3   #Rate of exploration
 
 #For plotting metrics
 all_epochs = []
 all_rewards = []
 
-num_episodes = 200
+num_episodes = np.power(10, 2)
 i = 0
 
 for ep in range(num_episodes):
@@ -72,16 +76,27 @@ for ep in range(num_episodes):
         
         
         if done:
-            print('Episode {}' .format(i), 'finished with {} reward' .format(tot_reward))
+            #print('Episode {}' .format(i), 'finished with {} reward' .format(tot_reward))
             all_rewards.append(tot_reward)
-            epsilon *= 0.9
+            #epsilon = np.maximum(0.1, epsilon * 0.9)
             #print("all rewards {}".format(all_rewards))
             i += 1
             break
-    
+
+print(q_table) 
+with open('test.csv', 'w') as fp:
+    for row in q_table:
+        for item in row:
+            fp.write("%s" % item)
+            fp.write(",")
+        fp.write("\n")
+    fp.close    
+
 with open('test.txt', 'w') as f:
     for item in all_rewards:
         f.write("%s\n" % item)
     f.close
 
-print(all_rewards)
+plt.plot(all_rewards)
+plt.show()
+#print(all_rewards)
